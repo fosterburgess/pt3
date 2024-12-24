b773176690d6cd96e917cfdeab2ca9045f20a75d
Michael Kimsal <mgkimsal@gmail.com>
2024-12-24 10:45:39 -0500

    feat: Add custom redirect logic after project creation
    Implemented a method to customize the redirect URL post-project creation. It prioritizes redirecting to the edit page if editable, falling back to the index page otherwise. This ensures a smoother user experience after creating a project.

--- app/Filament/Resources/ProjectResource/Pages/CreateProject.php
+++ app/Filament/Resources/ProjectResource/Pages/CreateProject.php
@@ -10,9 +10,23 @@ class CreateProject extends CreateRecord
 {
     protected static string $resource = ProjectResource::class;
 
+    protected static bool $canCreateAnother = false;
+
     protected function mutateFormDataBeforeCreate(array $data): array
     {
         $data['user_id'] = auth()->user()->id;
         return $data;
+    }
+
+
+    protected function getRedirectUrl(): string
+    {
+        $resource = static::getResource();
+
+        if ($resource::hasPage('edit') && $resource::canEdit($this->getRecord())) {
+            return $resource::getUrl('edit', ['record' => $this->getRecord(), ...$this->getRedirectUrlParameters()]);
+        }
+
+        return $resource::getUrl('index');
     }
 }
