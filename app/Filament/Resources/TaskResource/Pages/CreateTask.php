60bfc129bea41b4ad5364d49cbf0be603aea0960
Michael Kimsal <mgkimsal@gmail.com>
2024-12-26 16:39:28 -0500

    Add user_id to task creation data
    Ensure the authenticated user's ID is automatically assigned to the `user_id` field when creating a new task. This enforces proper task ownership and improves data integrity.

--- app/Filament/Resources/TaskResource/Pages/CreateTask.php
+++ app/Filament/Resources/TaskResource/Pages/CreateTask.php
@@ -9,4 +9,11 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateTask extends CreateRecord
 {
     protected static string $resource = TaskResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = auth()->user()->id;
+        return $data;
+    }
+
 }
