bf9b2ac2fbeb810f97e6c9c3d04f068233570b7f
Michael Kimsal <mgkimsal@gmail.com>
2024-12-28 13:38:32 -0500

    Add ActivityLogTrait for centralized activity logging
    Introduce a new trait leveraging Spatie's LogsActivity package to simplify and centralize activity logging. This provides a default implementation for generating log options using the `getActivitylogOptions()` method.

---
+++ app/Traits/ActivityLogTrait.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Traits;
+
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+trait ActivityLogTrait
+{
+    use LogsActivity;
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults();
+    }
+}
