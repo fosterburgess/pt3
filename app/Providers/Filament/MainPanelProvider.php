0aaa566be1094287c6fe7a616f125bf97ce83684
Michael Kimsal <mgkimsal@gmail.com>
2024-12-29 16:23:17 -0500

    refactor: remove unused widgets and related imports
    Cleaned up unused widget imports and explicit widget definitions in MainPanelProvider. This reduces clutter and aligns with dynamic widget discovery for better maintainability.

--- app/Providers/Filament/MainPanelProvider.php
+++ app/Providers/Filament/MainPanelProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers\Filament;
 
-use App\Filament\Resources\TaskResource\Widgets\TaskCalendarWidget;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -12,7 +11,6 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\View\PanelsRenderHook;
-use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -39,8 +37,6 @@ class MainPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
@@ -49,7 +45,8 @@ class MainPanelProvider extends PanelProvider
                 AuthenticateSession::class,
                 ShareErrorsFromSession::class,
                 VerifyCsrfToken::class,
-                SubstituteBindings::class,
+                SubstituteBindings
+                ::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
             ])
